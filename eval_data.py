7d6cd0cea1eb5e01c051c55d7480092c025b8d66
尹树雨 <shuyuyin@yinshuyus-MacBook-Pro.local>
2018-04-02 20:21:55 -0400

    fix bug in evaluation

--- eval_data.py
+++ eval_data.py
@@ -11,12 +11,16 @@ for file in dir_list:
     tmp_data = io.loadmat("./data/GMM/" + file)
     ccims = ["CCIm1", "CCIm2", "CCIm3"]
     segs = ["Seg1", "Seg2", "Seg3"]
+    print("start to calculate")
+    tmp_value = []
     for i in range(3):
         ccim = tmp_data[ccims[i]]
         seg = tmp_data[segs[i]]
         val = MyClustEvalRGB3(ccim, seg)
-        value.append(val)
+        tmp_value.append(val)
         print("value is %.3f" % val)
+    print("get the smallest value")
+    value.append(min(tmp_value))
 
 mean = numpy.mean(value)
 stdv = numpy.std(value)
